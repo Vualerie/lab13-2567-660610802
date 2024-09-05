6e7981181b83a49f57a9f66da2d1039bd177518b
Vualerie <jumper13577@gmail.com>
2024-09-05 11:19:36 +0700

    complete

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,12 +1,16 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
-
-export default function SearchResultPage() {
+import MovieRow from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
+import { movieNamePageParams } from "@lib/types";
+export default function SearchResultPage({ params }: any) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
-
+  const movieName = params.searchInput.replaceAll("%20"," ");
+  const selMovie = movieDB.filter((movie) => movie.title.toLowerCase().includes(movieName.toLowerCase()));
+  if (!selMovie) {
+    return (<p className="text-center">{"Movie is not found 😥 "+ movieName}</p>);
+  }
   /*
   tip2 : Use "includes" string method to check substring
   Example : "ABC".includes("AB") -> return true
@@ -21,10 +25,20 @@ export default function SearchResultPage() {
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {movieName} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">Found {selMovie.length} result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {selMovie.map((movie, i) => (
+          <MovieRow
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            detail={movie.detail}
+            rating={movie.rating}
+            number={i + 1}
+          />
+        ))}
     </div>
   );
 }
