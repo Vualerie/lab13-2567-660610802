108270efcbdb436a17ede27be63166f168bdb24e
Vualerie <jumper13577@gmail.com>
2024-09-05 11:24:13 +0700

    finish

---
+++ src/app/search/[searchInput]/page.js
@@ -0,0 +1,38 @@
+"use client";
+
+import MovieRow from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
+
+export default function SearchResultPage({ params }) {
+  //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
+  const processedSearchInput = String(params.searchInput).replaceAll(
+    "%20",
+    " "
+  );
+  const moviesList = movieDB.filter((x) =>
+    x.title.toLowerCase().includes(processedSearchInput.toLowerCase())
+  );
+  console.log(moviesList);
+
+  return (
+    <div>
+      <p className="fw-bold fs-4 text-center my-0">
+        Searching &quot; {processedSearchInput} &quot;
+      </p>
+      <p className="fw-bold fs-4 text-center">
+        Found {moviesList.length} result(s)
+      </p>
+
+      {moviesList.map((x, i) => (
+        <MovieRow
+        key={x.id}
+        id={x.id}
+        title={x.title}
+        detail={x.detail}
+        rating={x.rating}
+        number={i + 1}
+      />
+      ))}
+    </div>
+  );
+}
